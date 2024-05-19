354d95d19fbfaff3b33bbf3004858bb26517e4f1
brenog4briel <breno-112011@hotmail.com>
2024-05-19 11:16:43 -0300

    feat: adicao do consumo da api e estilizacao dos filmes resgatados

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,25 +1,80 @@
+import { useEffect, useState } from "react";
 import styles from "./home.module.css"
 import { BsSearch } from "react-icons/bs";
+import {Rating} from "@mui/material"
 
+const API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMzU5MDE4NTBlYTYzMGE4Y2RkZmIyYjU3M2M5MjBmZiIsInN1YiI6IjY2NDhiNTVkYTBmNzE0NGU0NDkyNTZjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Kfvpp9RCd-x6hrX8QlGgbqgOyyFRF7psvKsrpLGdiSk"
+
+interface MovieProps {
+    adult: boolean;
+    backdrop_path: string;
+    genre_ids: number[];
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    poster_path: string;
+    popularity: number;
+    release_date: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+}
+
+interface DataProps {
+  page:number;
+  results: MovieProps[];
+  total_pages: number;
+  total_results:number;
+}
 export function Home() {
+
+  const [movies,setMovies] = useState<MovieProps[]>();
+
+  useEffect(() => {
+    getData()
+  })
+
+  function formatDate(date:string) {
+    const newData = new Date(date)
+    return newData.toLocaleDateString("pt-br");
+  }
+
+  async function getData() {
+    fetch("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc",{
+      method:"get",
+       headers: {
+        'Content-type': 'application/json',
+        'Authorization': `Bearer ${API_TOKEN}`, // notice the Bearer before your token
+    },
+    })
+    .then((res) => res.json())
+    .then((data : DataProps) => setMovies(data.results))
+  }
+
   return (
     <main className={styles.container}>
       <form className={styles.form} action="">
         <input type="search" name="" id="" placeholder="Digite o nome de filme que deseja procurar"/>
-        <button type="submit">
-          <BsSearch size={30} color="white"/>
+        <button type="button" onClick={() => formatDate("2024-01-18")}>
+          <BsSearch size={25} color="white"/>
         </button>
       </form>
 
       <div className={styles.movie_container}>
-        <div className={styles.movie_item}>1</div>
-        <div className={styles.movie_item}>2</div>
-        <div className={styles.movie_item}>3</div>
-        <div className={styles.movie_item}>4</div>
-        <div className={styles.movie_item}>5</div>
-        <div className={styles.movie_item}>6</div>
-        <div className={styles.movie_item}>7</div>
-        <div className={styles.movie_item}>8</div>
+        {movies ? movies.map((movie) => (
+          <div key={movie.id} className={styles.movie_item}>
+              <img className={styles.movie_poster} src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt=""/>
+              <div className={styles.movie_info}>
+                <div className={styles.container_title}>
+                 <h3>{movie.title}</h3>
+                 <Rating defaultValue={Math.round(movie.vote_average)} precision={1} max={10} readOnly/>
+                </div>
+                <p>Data de lançamento: {formatDate(movie.release_date)}</p>
+              </div>
+          </div>
+        )) : <p>carregando...</p>}
       </div>
     </main>
   )
