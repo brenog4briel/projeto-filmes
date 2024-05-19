f6bee28b9cb742ca490ade6e196b5156ab6f30af
brenog4briel <breno-112011@hotmail.com>
2024-05-18 21:22:29 -0300

    feat: layout inicial da pagina home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,26 @@
+import styles from "./home.module.css"
+import { BsSearch } from "react-icons/bs";
+
 export function Home() {
   return (
-    <h1>Página inicial</h1>
+    <main className={styles.container}>
+      <form className={styles.form} action="">
+        <input type="search" name="" id="" placeholder="Digite o nome de filme que deseja procurar"/>
+        <button type="submit">
+          <BsSearch size={30} color="white"/>
+        </button>
+      </form>
+
+      <div className={styles.movie_container}>
+        <div className={styles.movie_item}>1</div>
+        <div className={styles.movie_item}>2</div>
+        <div className={styles.movie_item}>3</div>
+        <div className={styles.movie_item}>4</div>
+        <div className={styles.movie_item}>5</div>
+        <div className={styles.movie_item}>6</div>
+        <div className={styles.movie_item}>7</div>
+        <div className={styles.movie_item}>8</div>
+      </div>
+    </main>
   )
 }
